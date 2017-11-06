adc42b27cd211e4148ea0b487130fa2ffba40f43
CloudNiner <andrew@cloudniner.co>
2017-11-06 10:31:40 -0500

    Handle short missions and no missions

--- app/scripts/views/museum/mission-directive.js
+++ app/scripts/views/museum/mission-directive.js
@@ -7,12 +7,17 @@
         var SHORT_CHAR_LIMIT = 350;
         var ctl = this;
         ctl.expanded = false;
+        ctl.showMore = false;
         ctl.$onChanges = $onChanges;
 
         function $onChanges(changes) {
             var mission = changes.mission.currentValue;
-            if (mission) {
+            if (mission && mission.length) {
                 ctl.shortMission = mission.substring(0, SHORT_CHAR_LIMIT);
+                ctl.showMore = !(mission.length === ctl.shortMission.length);
+                if (ctl.showMore) {
+                    ctl.shortMission = ctl.shortMission + '...';
+                }
             }
         }
     }
